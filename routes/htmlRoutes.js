02fd00fd67473b32ca837d472948b910302dced3
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-12-20 12:47:20 -0700

    changed syntax of html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,20 +4,19 @@ const path = require("path");
 
 module.exports = function(app) {
 
-// this is where my HTML 'GET' requests go.
-
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-
-app.get("*", function(req, res) {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 
 }
