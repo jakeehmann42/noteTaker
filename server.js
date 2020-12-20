2ff112c27c854817e4d4f21f9eca763ba7a4d287
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-12-20 12:56:17 -0700

    application is working and is ready to be deployed.

--- server.js
+++ server.js
@@ -5,20 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 
-app.use();
-app.use();
-app.use();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-
-apiRoute();
-htmlRoute();
-
-
-
-
-
-
-
+apiRoute(app);
+htmlRoute(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
